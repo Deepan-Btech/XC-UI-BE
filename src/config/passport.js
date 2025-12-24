3124d9d6326c363b826045c6c162cbdfe73c4233
Deepan_R <deepandeepan6098@gmail.com>
2025-12-24 11:36:55 +0530

    env updated

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,13 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
+const CALLBACK_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://xplocode.onrender.com/auth/google/callback"
+    : "http://localhost:4000/auth/google/callback";
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: CALLBACK_URL
     },
     async (_, __, profile, done) => {
       try {
